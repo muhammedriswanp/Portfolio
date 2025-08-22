962f4aca14ec691b768a9bad5da323ca9841214f
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-08-22 21:02:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function sendEmail(event) {
+    event.preventDefault(); 
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+    const serviceID = "service_kr2ihc4";
+const templateID = "template_i8lvrd9";
+    emailjs.send(serviceID, templateID,params)
+        .then(
+            res => {
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('message').value = '';
+                console.log(res);
+                alert("Your message has been sent successfully!");
+            }
+        )
+        .catch(err => console.log(err));
+}
